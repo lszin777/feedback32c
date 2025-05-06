77e065995b0135ea28a17cc05e27902a20b9f40e
gamerinfinitlucas <gamerinfinitlucas@gmail.com>
2025-05-06 02:34:11 +0000

    Criação da função para atualizar o card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,73 @@
-import iconStar from "./assets/icon-star.svg"
+import iconStar from "./assets/icon-star.svg";
+import { useState } from "react";
 
 export function App() {
-  return(
+  const [submited, setSubmited] = useState(false);
+  const [rateNote, setRateNote] = useState(0); // Correção feita aqui
+
+  function handleSubmit() {
+    if (rateNote !== 0) {
+      setSubmited(true);
+      return
+    }
+
+    alert("Por favor, escolha um botão de classificação!")
+  }
+
+  function handleChangeRateNote(value) {
+    setRateNote(value)
+  }
+
+  return (
     <div className="h-svh bg-very-dark-blue text-white flex justify-center items-center px-6">
-      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
-        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-          <img src={iconStar} alt="icone da estrela" />
+      {submited === false ? (
+        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
+          <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+            <img src={iconStar} alt="icone da estrela" />
+          </div>
+          <h1 className="font-bold text-2xl mb-2.5">Como foi o atendimento?</h1>
+          <p className="text-light-grey text-sm leading-1 mb-6">
+            Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito
+            seu feedback para podermos melhorar nosso atendimento!
+          </p>
+
+          <div className="flex justify-between mb-6">
+            <input
+              type="button"
+              value={1}
+              className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleChangeRateNote(1)} />
+            
+            <input
+              type="button"
+              value={2}
+              className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleChangeRateNote(2)} />
+            
+            <input
+              type="button"
+              value={3}
+              className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleChangeRateNote(3)} />
+            
+            <input
+              type="button"
+              value={4}
+              className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleChangeRateNote(4)} />
+            
+            <input
+              type="button"
+              value={5}
+              className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleChangeRateNote(5)} />
+            
+          </div>
+          <button
+            className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1"
+            onClick={handleSubmited}
+          >
+            Prosseguir
+          </button>
         </div>
-        <h1 className="font-bold text-2xl mb-2.5">Como foi o atendimento?</h1>
-        <p className="text-light-grey text-sm leading-1 mb-6">Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
-      
-        <div className="flex justify-between mb-6">
-          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold"/>
-          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold"/>
-          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold"/>
-          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold"/>
-          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10 rounded-full text-sm text-medium-grey font-bold"/>
-        </div>
-        <button className="bg-orange">Prosseguir</button>
-      </div>
+      ) : (
+        <p>Outro Codigo</p>
+      )}
     </div>
-  )
+  );
 }
